7ef19d0c422339c506fbe397b8604beada95eece
Henrique S. Mendes <124588969+HenriqueScloud@users.noreply.github.com>
2024-01-09 16:02:58 -0300

    NEW COMMIT - JS ( KENZIE ACADEMY

---
+++ Módulo 02/EXERCÍCIOS/script4.js
@@ -0,0 +1,25 @@
+function AddStringArray(str){
+    let arr = [];
+    if(str.length >= 5){
+        arr.push(str)
+    }
+    return arr
+}
+
+function getNameFromArrayPosition(num){
+    let arr2 =['Hermanoteu', 'Olonéia','Migalatéia','Godart']
+
+    if (num >=0 && num<arr2.length){
+        return arr2[num]
+    }
+    return 'valor não encontrado !'
+}
+
+function verifyParImpar(num){
+    let arr3 = [1,2,3,4,5,6,7,8,9,10]
+
+    if (arr3[num]%2==0){
+        return `${arr3[num]} é par`
+    }
+    return `${arr3[num]} é impar`
+}

98a40abfb7a6aa7589f123f82391bf959ea5d4da
Henrique S. Mendes <124588969+HenriqueScloud@users.noreply.github.com>
2024-01-11 13:19:21 -0300

    new commit - js ( kenzie academy ) / TCM

--- Módulo 02/TCM - palindromo e minMax/script.js
+++ Módulo 02/TCM - palindromo e minMax/script.js
@@ -1,7 +1,23 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(text){
+    text = text.toLowerCase().replaceAll(' ','')
+    let palindromo = '';
+    for (let i=0;i<text.length;i++){
+        palindromo = text[i] + palindromo
+    }
+    return palindromo === text;
+    
 }
-
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let max = arr[0];
+    let min = arr[0];
+    for(let i=1; i < arr.length; i++) {
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        else if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    let novoArray=[min,max]
+    return novoArray;
 }

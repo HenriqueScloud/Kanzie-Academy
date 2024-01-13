b660a474b1179932739d0d9e119ebfe547ebf631
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-01-13 17:54:52 -0300

    NEW COMMIT - JS (TESTE)

--- Módulo 02/TCM - palindromo e minMax/script.js
+++ Módulo 02/TCM - palindromo e minMax/script.js
@@ -21,3 +21,10 @@ function arrayMaxMin(arr){
     let novoArray=[min,max]
     return novoArray;
 }
+
+// olha como o mundo é 
+// sempre que melhoramos vemos como eramos infriores a pessoas que sabiam muito mais que a gente sabe atualmente e futuramente saberemos mais do que muito e menos que poucos 
+
+
+a vide sempre nos da uma nova oportunidade de mudar nossos habitos mais precisamos aceitar isso em nossas vidas para que possamos realmente evoluir para melhor
+e

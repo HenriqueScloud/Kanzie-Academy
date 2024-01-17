373a9f37cbf610c54dd85830587bbfc768ad4886
HenriqueScloud <henriquesilvamendes076@gmail.com>
2024-01-17 10:58:35 -0300

    NEW COMMIT -  Projeto movies (kenzie) / js

---
+++ Módulo 03/Rascunhos aula/introdução ao DOM/script.js
@@ -0,0 +1,8 @@
+const novoElemento = document.createElement('h1');
+const novoElementoText = document.createElement('p');
+novoElemento.innerText = 'OLá, Mundo!';
+novoElementoText.innerText = 'Esse texto e de exemplo para o paragrafo do h1 a cima !'
+
+const corpo = document.querySelector('body');
+//corpo.appendChild(novoElemento);
+//corpo.appendChild(novoElementoText);

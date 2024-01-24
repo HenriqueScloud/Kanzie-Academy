0173b379dd5230f4ca5fbc6a82792abf090a1421
HenriqueScloud <henriquesilvamendes076@gmail.com>
2024-01-24 11:55:53 -0300

    NEW COMMIT- EXER. CHAT

---
+++ Módulo 03/Rascunhos aula/Semana 01/rascunho de chat/script.js
@@ -0,0 +1,32 @@
+// Imprima no console cada numero do array elevado ao quadrado 
+const numeros = [1, 2, 3, 4, 5];
+// Seu código aqui
+
+// crie um novo array com o cumprimento de cada nome do array 
+const nomes = ['Alice', 'Bob', 'Charlie', 'David'];
+
+// Seu código aqui
+
+
+
+
+//crie um novo array contendo apenas os numeros pares do array abaixo 
+
+const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Seu código aqui
+
+
+
+//encontre a primeira palavra com mais de 5 letras do array abaixo 
+const palavras = ['maçã', 'banana', 'laranja', 'uva', 'abacaxi'];
+
+// Seu código aqui
+
+
+
+
+// calcule a soma de todos os numero do array abaixo !
+const numeros3 = [1, 2, 3, 4, 5];
+
+// Seu código aqui

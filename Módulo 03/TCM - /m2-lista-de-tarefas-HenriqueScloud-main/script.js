ff8b2da50ece64affbbc772e828df66517e8e410
HenriqueScloud <henriquesilvamendes076@gmail.com>
2024-01-25 17:19:22 -0300

    new commit - tcm lista de tarefas (  js )

--- Módulo 03/TCM - /m2-lista-de-tarefas-HenriqueScloud-main/script.js
+++ Módulo 03/TCM - /m2-lista-de-tarefas-HenriqueScloud-main/script.js
@@ -10,3 +10,47 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+function renderElements(array){  
+}
+
+function createTaskItem(array){
+    
+  const newObject = array.map((item) => {
+
+      const li = document.createElement('li');
+      li.classList.add('task__item');
+
+      const div = document.createElement('div');
+      div.classList.add("task-info__container");
+
+      const span = document.createElement("span");
+      span.classList.add("task-type");
+
+      const p = document.createElement('p');
+      p.innerText = item.title;
+
+      const button  = document.createElement("button");
+      button.classList.add('task__button--remove-task')
+      button.innerHTML = '<img src="assets/trash-icon.svg" alt="limpar tarefa"></img>';
+
+      if (item.type == 'normal'){
+        span.classList.add('span-normal')
+      }
+      else if(item.type == 'urgente'){
+        span.classList.add('span-urgent')
+
+      }
+      else{
+        span.classList.add('span-important')
+      }
+      li.append(div,button)
+      div.append(span,p)
+
+  })
+}
+renderElements(tasks);
+
+
+
+

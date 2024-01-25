b3434a22fbd243d337fa69617cd2cb82b4fcf1fe
HenriqueScloud <henriquesilvamendes076@gmail.com>
2024-01-25 14:03:07 -0300

    NEW COMMIT - EXPLICAÇÃO

--- Módulo 03/Rascunhos aula/Semana 03/S3-12 - Eventos de formularios/script.js
+++ Módulo 03/Rascunhos aula/Semana 03/S3-12 - Eventos de formularios/script.js
@@ -1,17 +1,13 @@
 function valueForm(){
-
     const form = document.querySelector("form")
-    //console.log(form)
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         const nome = document.querySelector('#nome')
         const senha = document.querySelector('#senha')
         const email = document.querySelector('#email')
-
         console.log(nome.value)
         console.log(email.value)
         console.log(senha.value)
     })
-
 }
-valueForm();
+valueForm(); // capiturando os valores de dentro do input e do select.

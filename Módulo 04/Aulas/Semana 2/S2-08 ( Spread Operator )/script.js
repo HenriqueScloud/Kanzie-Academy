b391865c16959ce04815710f57d535f193e71a4d
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-02-18 20:38:47 -0300

    NEW COMMIT - S2-08 / Spread Operator (.js)

--- Módulo 04/Aulas/Semana 2/S2-08 ( Spread Operator )/script.js
+++ Módulo 04/Aulas/Semana 2/S2-08 ( Spread Operator )/script.js
@@ -6,6 +6,14 @@ console.log(objeto3);
 
 
 
+// Transformando uma string em um array com io spread Operator 
+
+const string = 'javascript';
+const arrays1 = [...string]
+console.log(arrays1)
+
+
+
 // O spread operator ṕe uma sintaxe dentro da desestruturação que permite que o  conteúdo de um array ou propriedades de um objeto sejam espalhado dentro de um outro objeto. podendo criar um objeto com tudo que tem dentro do dois ou mais objetos 
 
 

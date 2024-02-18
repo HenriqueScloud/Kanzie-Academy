e0be06eaaa61448fa4b68ed5a4882325a50d9c76
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-02-18 16:25:40 -0300

    NEW COMMIT - S2-05 / MÉTODO REDUCE (js)

---
+++ Módulo 04/Aulas/Semana 2/S2-05 ( Método Reduce  )/script.js
@@ -0,0 +1,11 @@
+const persons = [
+    { name: 'John', age: 30 },
+    { name: 'Jane', age: 25 },
+    { name: 'Bob', age: 40 },
+    { name: 'Alice', age: 35 }
+];
+const totalAges = persons.reduce((accumulator, person) => {
+    accumulator + person.age;
+    console.log(accumulator)
+,0});
+console.log(totalAges);

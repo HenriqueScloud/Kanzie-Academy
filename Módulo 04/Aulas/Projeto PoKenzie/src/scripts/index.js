6bc60b134571238513755438c857ac2ab675e7bf
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-02-25 21:12:27 -0300

    NEW COMMIT - PROJETO POKENZIE (INDEX.JS)

--- Módulo 04/Aulas/Projeto PoKenzie/src/scripts/index.js
+++ Módulo 04/Aulas/Projeto PoKenzie/src/scripts/index.js
@@ -1,40 +1,44 @@
 
-import { pokemonList } from "./pokeDatabase.js";
+//import { pokemonList } from "./pokeDatabase.js";
+import { mountPokemonArray } from "./api.js";
+
+import  { handleDarkMode } from './theme.js';
+
 
 function createPokemonCard({id,name,img,types,hp,attack,defense,speed}){
     const liCard = document.createElement('li');
     liCard.classList.add('card');
     const iconState = 'ifavoritado';
     const favIcon = iconState === 'favoritado' 
-            ? `<i class="card__fav fa-solid fa-star"></i>`
-            : `<i class="card__fav fa-solid fa-star"></i>`;
+    ? `<i class="card__fav fa-solid fa-star"></i>`
+    : `<i class="card__fav fa-solid fa-star"></i>`;
     
     liCard.innerHTML = `
     ${favIcon}
     <p class="card__hp">
-        <span>HP</span>
-        ${hp}
+    <span>HP</span>
+    ${hp}
     </p>
     <img
-        class="card__image"
-        src="${img}"
-        alt="Imagem do Pokemon ${name}"
+    class="card__image"
+    src="${img}"
+    alt="Imagem do Pokemon ${name}"
     />
     <h2 class="card__name">${name}</h2>
     <small class="card__type">${types}</small>
     <ul class="card__stats">
-        <li class="card__stat">
-        <h3 class="stat__value">${attack}</h3>
-        <p class="stat__type">ATQ</p>
-        </li>
-        <li class="card__stat">
-        <h3 class="stat__value">${defense}</h3>
-        <p class="stat__type">DEF</p>
-            </li>
-        <li class="card__stat">
-        <h3 class="stat__value">${speed}</h3>
-        <p class="stat__type">VEL</p>
-        </li>
+    <li class="card__stat">
+    <h3 class="stat__value">${attack}</h3>
+    <p class="stat__type">ATQ</p>
+    </li>
+    <li class="card__stat">
+    <h3 class="stat__value">${defense}</h3>
+    <p class="stat__type">DEF</p>
+    </li>
+    <li class="card__stat">
+    <h3 class="stat__value">${speed}</h3>
+    <p class="stat__type">VEL</p>
+    </li>
     </ul>`
     return liCard;
 };// criando os cards 
@@ -42,15 +46,21 @@ function createPokemonCard({id,name,img,types,hp,attack,defense,speed}){
 function renderPokemonCards(pokemonArray){
     const ulCards = document.querySelector('.cards');
     pokemonArray.forEach((pokemonInfo)=>{
-    const pokemonCard = createPokemonCard(pokemonInfo)
-    ulCards.appendChild(pokemonCard);
-});
+        const pokemonCard = createPokemonCard(pokemonInfo)
+        ulCards.appendChild(pokemonCard);
+    });
 }; // renderizando os cards para atualizar a lista de  Pokemons na tela com o banco de dados javascript 
 
+async function main(){
+    //responsavel pela renderização dos cards 
+    const pokemonArray = await mountPokemonArray(100);
+    renderPokemonCards(pokemonArray); 
+
+    // responsavel pelo dark mode 
+    handleDarkMode();
+    
+};
+main();
 
 
-renderPokemonCards(pokemonList); 
-
-import  { handleDarkMode } from './theme.js';
-handleDarkMode()
 

7179ceb9c67acaba7c71a83f4bcbd9546db3a93d
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-02-28 10:39:03 -0300

    NEW COMMIT - PROJETO OPEN MUSIC ( THEME.JS )  / "Tarefa 4 concluída"

--- Módulo 04/TCM/src/script/theme.js
+++ Módulo 04/TCM/src/script/theme.js
@@ -1,12 +1,24 @@
-export function selectedFiltroGenre(){
-    const li = document.querySelectorAll('.filtro__genre-btn');
-    li.forEach( item => {
-        item.addEventListener( 'click', (event)=>{ 
-            li.forEach(item => {item.classList.remove('filtro__genre-btn--ativo')});
-            const btn = event.target;
-            btn.classList.add('filtro__genre-btn--ativo');
-        });
-    });
-};
+export function ThemeDarkMode(){
 
-// sele
+    const btnDarkMode = document.querySelector(`.header__theme-btn`); 
+    const html = document.querySelector('html');
+
+    let darkMode = false;
+
+    function themeDark(){
+        darkMode = !darkMode;
+        btnDarkMode.classList.toggle('header__theme-btn--dark');
+        html.classList.toggle('darkMode');        
+        localStorage.setItem("theme", JSON.stringify(darkMode));  
+    }
+    btnDarkMode.addEventListener("click", themeDark);
+
+    function themeJustify(){
+        darkMode = JSON.parse(localStorage.getItem("theme")); 
+        if (darkMode) {
+            btnDarkMode.classList.add('header__theme-btn--dark');
+            html.classList.add('darkMode');
+        }
+    }
+    themeJustify();
+};

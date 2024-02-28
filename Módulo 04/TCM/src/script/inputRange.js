bdc31627571a847e1704782a4666fcc489e62045
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-02-28 10:38:25 -0300

    NEW COMMIT - PROJETO OPEN MUSIC ( INPUTRANGE.JS )  / "Tarefa 4 concluída"

--- Módulo 04/TCM/src/script/inputRange.js
+++ Módulo 04/TCM/src/script/inputRange.js
@@ -1,3 +1,6 @@
+import { albumList } from "./albumsDatabase.js";
+import { renderAlbumListCards}  from "./index.js";
+
 export function applyInputRangeStyle() {
     const inputRange = document.querySelector("#price__range-input");
     
@@ -5,6 +8,37 @@ export function applyInputRangeStyle() {
         const currentInputValue = event.target.value;
         const runnableTrackProgress = (currentInputValue / inputRange.max) * 100;
         
-        inputRange.style.background = `linear-gradient(to right, var(--brand-1) ${runnableTrackProgress}%, var(--gray-8) ${runnableTrackProgress}%)`;
+        inputRange.style.background = `linear-gradient(to right, var(--brand-1) ${runnableTrackProgress}%, var(--gray-6) ${runnableTrackProgress}%)`;
     });
 };
+
+
+export function handleStatFilters(){
+    const filtreInputRange = document.querySelector('#price__range-input');
+    filtreInputRange.addEventListener('input', (event)=>{
+        const currentInputStat = event.target.previousElementSibling;
+
+        const filtroGenre = document.querySelector('.filtro__genre-btn--ativo').innerText;
+        
+        const currentInputStatSpan = currentInputStat.querySelector('.price-range-value');
+        const  currentInputDataValue = event.target.valueAsNumber;
+        currentInputStatSpan.innerText = currentInputDataValue;
+
+        const arrayAlbunsFiltred = albumList.filter(({price,genre})=>{
+            if(filtroGenre == 'Todos'){
+                return price <= currentInputDataValue;
+            }
+            return (price <= currentInputDataValue && 
+                    genre == filtroGenre || genre === 'Todos' && price  <= currentInputDataValue );
+            });
+
+            renderAlbumListCards(arrayAlbunsFiltred);
+        });
+    };
+    
+    export function handleFiltredButtonsGenre(){
+        const  botoesGeneros = document.querySelector('.filtro__genre-btn--ativo');
+        botoesGeneros.addEventListener('click', handleStatFilters);
+
+    ;}
+handleFiltredButtonsGenre()

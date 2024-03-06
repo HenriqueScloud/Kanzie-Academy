024cf8aeff275594369dcb1144637cce35707a01
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-06 15:45:06 -0300

    NEW POST - PROJETO BORA COMER PIZZA ( DATABASE.JS )

---
+++ Módulo 05/Projetos de treino/bora-comer-pizzas/src/Database/DataBase.js
@@ -0,0 +1,34 @@
+
+import img1 from "../assets/images/pizza1.jpg";
+import img2 from "../assets/images/pizza2.jpg";
+import img3 from "../assets/images/pizza3.jpg";
+import img4 from "../assets/images/pizza4.jpg";
+import img5 from "../assets/images/pizza5.jpg";
+
+export const pizzasList = [
+    {
+        id: 1,
+        img: img1,
+        title: 'pizza 1',
+    },
+    {
+        id: 2,
+        img: img2,
+        title: 'pizza 2',
+    },
+    {
+        id: 3,
+        img: img3,
+        title: 'pizza 3',
+    },
+    {
+        id: 4,
+        img: img4,
+        title: 'pizza 4',
+    },
+    {
+        id: 5,
+        img: img5,
+        title: 'pizza 5',
+    },
+]

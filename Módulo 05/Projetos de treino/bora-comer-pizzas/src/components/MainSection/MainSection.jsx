d5228f75826baefc093c26fe60063b95792f11f9
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-06 15:44:34 -0300

    NEW POST - PROJETO BORA COMER PIZZA ( MAINSECTION.JSX )

---
+++ Módulo 05/Projetos de treino/bora-comer-pizzas/src/components/MainSection/MainSection.jsx
@@ -0,0 +1,23 @@
+
+import { pizzasList } from "../../Database/DataBase.js";
+export const MainSection = () =>{
+
+    return(
+        <>
+            <section>
+                <h2>Confira o Cardápio</h2>
+                <ul>
+                    {pizzasList.map((pizza, index) => {
+                        return(
+                            <li key={index}> 
+                            <h1>{pizza.id}</h1>
+                                <img src={pizza.img} alt="" />
+                                <h3>{pizza.title}</h3>
+                            </li>      
+                    )
+                    })}
+                </ul>
+            </section>
+        </>
+    )
+}

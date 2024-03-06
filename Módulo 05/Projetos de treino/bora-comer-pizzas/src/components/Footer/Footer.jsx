8e80c580924ab715cc840c682bac27d3685eb416
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-06 15:43:27 -0300

    NEW POST - PROJETO BORA COMER PIZZA ( FOOTER.JSX )

---
+++ Módulo 05/Projetos de treino/bora-comer-pizzas/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import logo from "../../assets/icons/logo.png";
+
+
+export const Footer = () =>{
+
+    return(
+        <>
+            <footer>
+                <figure>
+                    <img src={logo} alt="" />
+                </figure>
+                <p>@Henrique S.</p>
+            </footer>
+        </>
+    )
+}

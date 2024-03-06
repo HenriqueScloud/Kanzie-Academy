1211ffc90ed06a9672c563ec36fb3811f9da69d4
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-06 15:44:20 -0300

    NEW POST - PROJETO BORA COMER PIZZA ( HEADER.JSX  )

---
+++ Módulo 05/Projetos de treino/bora-comer-pizzas/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import logo from "../../assets/icons/logo.png" 
+
+
+export const Header = () =>{
+    return (
+        <>
+            <header>
+                <figure>
+                <img src={logo} alt="logo" />
+                </figure>
+            </header>
+        </>
+    )
+}

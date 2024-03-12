e65349b2ea6b4f26ba172f0d8c09dd00f662463f
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-11 21:36:59 -0300

    NEW COMMIT - AULA 11/03 - PROJETO FASHION STORY ( NEWSLATTERSECTION.JSX )

---
+++ Módulo 05/Projetos de treino/fashion-story/src/components/sections/NewsLatterSection/NewsLetterSection.jsx
@@ -0,0 +1,10 @@
+export const NewsLatterSection = () =>{
+    return (
+        <form>
+            <h2>INSCREVA-SE NA NEWSLETTER</h2>
+            <input type="email" name="" id="" placeholder="E-MAIL" />
+            
+            <button>CADASTRE-SE</button>
+        </form>
+    )
+}

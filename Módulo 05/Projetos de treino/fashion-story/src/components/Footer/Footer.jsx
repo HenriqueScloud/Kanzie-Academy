f66e3642ecc7d0d6a4dd894a86b24ab04f7241da
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-11 21:36:17 -0300

    NEW COMMIT - AULA 11/03 - PROJETO FASHION STORY ( FOOTER.JSX )

---
+++ Módulo 05/Projetos de treino/fashion-story/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () =>{
+    return (
+        <footer>
+            <p>
+            Todos os direitos reservados - Kenzie Academy Brasil
+            </p>
+        </footer>
+    )
+}

53416db08c4d8fd5cf233eda60e9953d483f6fef
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-11 21:36:30 -0300

    NEW COMMIT - AULA 11/03 - PROJETO FASHION STORY ( HEADER.JSX )

---
+++ Módulo 05/Projetos de treino/fashion-story/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../../assets/icon/FashionStoreLogo.svg"
+
+export const Header = () =>{
+    return (
+        <header>
+        <img src={logo} alt="" />
+        </header>
+    )
+}

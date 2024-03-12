312b882fd14da3eb1d90ac7219b9cf1bdbd4d5f3
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-11 21:37:55 -0300

    NEW COMMIT - AULA 11/03 - PROJETO FASHION STORY ( PRODUTODATABASE.JS )

---
+++ Módulo 05/Projetos de treino/fashion-story/src/data/ProdutoDataBase.js
@@ -0,0 +1,31 @@
+import ProductImg1 from "../assets/Rectangle 4.png";
+import ProductImg2 from "../assets/Rectangle 6.png";
+import ProductImg3 from "../assets/Rectangle 7.png";
+import ProductImg4 from "../assets/Rectangle 8.png";
+
+export const products = [
+    {
+        id: 1,
+        image: ProductImg1,
+        title: "Blazer Branco Elegante",
+        price: 490.0,
+    },
+    {
+        id: 2,
+        image: ProductImg2,
+        title: "Sapatos Amarelos",
+        price: 340.0,
+    },
+    {
+        id: 3,
+        image: ProductImg3,
+        title: "Blazer Laranja",
+        price: 320.0,
+    },
+    {
+        id: 4,
+        image: ProductImg4,
+        title: "Calça Preta",
+        price: 190.0,
+    },
+];

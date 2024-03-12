fb2af3df29a056ef7e09ba291306aa709ea0f4bc
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-11 21:35:06 -0300

    NEW COMMIT - AULA 11/03 - PROJETO FASHION STORY ( APP.JSX )

--- Módulo 05/Projetos de treino/fashion-story/src/App.jsx
+++ Módulo 05/Projetos de treino/fashion-story/src/App.jsx
@@ -1,8 +1,29 @@
+import "./styles/index.css";
+
+
+import { Header } from "./components/Header/Header";
+import { BannnerSection } from "./components/sections/BannerSection/BannerSection";
+import { ProductSection } from "./components/sections/ProductsSection/ProductsSection";
+import { NewsLatterSection } from "./components/sections/NewsLatterSection/NewsLetterSection";
+import { Footer } from "./components/Footer/Footer";
+
+
 function App() {
 
   return (
     <>
-      
+    <h1>ola mundo </h1>
+
+      <Header/>
+
+      <main>
+
+        <BannnerSection />
+        <ProductSection />
+        <NewsLatterSection />
+      </main>
+
+      <Footer/>
     </>
   )
 }

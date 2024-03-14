2daab7c7b1252ab464b33f03fddb3343dfd1cfb7
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-14 10:16:43 -0300

    NEW COMMIT - ESTILIZANDO PROJETO FASHION STORY ( PRODUCTCARD.JSX )

--- Módulo 05/Projetos de treino/fashion-story/src/components/sections/ProductsSection/ProductCard/ProductCard.jsx
+++ Módulo 05/Projetos de treino/fashion-story/src/components/sections/ProductsSection/ProductCard/ProductCard.jsx
@@ -1,9 +1,9 @@
-
+import style from "../style.module.css"
 // eslint-disable-next-line react/prop-types
 export const ProductCard = ({ image, title, price} ) => {
 
     return (
-        <li>
+        <li className={style.card}>
             <img src={image} alt="Product Image" />
             <h3 className='title3'>{title}</h3>
             <p  className='price'>R$ {price}</p>

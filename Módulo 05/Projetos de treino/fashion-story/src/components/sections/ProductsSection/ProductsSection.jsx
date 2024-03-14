526744f393a62a5baf3788f12f83311790593697
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-13 22:19:39 -0300

    NEW COMMIT - AULA 13/03 ( PRODUCTSECTION.JSX )

--- Módulo 05/Projetos de treino/fashion-story/src/components/sections/ProductsSection/ProductsSection.jsx
+++ Módulo 05/Projetos de treino/fashion-story/src/components/sections/ProductsSection/ProductsSection.jsx
@@ -1,22 +1,15 @@
 import { ProductCard } from "./ProductCard/ProductCard";
-
-import ProductImage1 from "../../../assets/image/Rectangle 4.png";
-import ProductImage2 from "../../../assets/image/Rectangle 6.png";
-import ProductImage3 from "../../../assets/image/Rectangle 7.png";
-import ProductImage4 from "../../../assets/image/Rectangle 8.png";
+import { products } from "../../../data/ProdutoDataBase";
+import style from "./style.module.css";
 
 
 export const ProductSection = () =>{
     return (
-        <section>
-            <h2>PRODUTOS EM DESTAQUE</h2>
+        <section className={style.productSection}>
+            <h2 className='title2'>PRODUTOS EM DESTAQUE</h2>
             <ul>
-            {/* desafio de criar um loop para  repetir a div abaixo */}
-                <ProductCard image={ProductImage1} title='Blaze Branco Elegante' price='490,00' />
-                <ProductCard image={ProductImage2} title='Sapatos Amarelos' price='340,00' />
-                <ProductCard image={ProductImage3} title='Blazer Laranja' price='320,00' />
-                <ProductCard image={ProductImage4} title='Calça Preta' price='140,00' />
-
+                
+                {products.map(({id,image,title,price}) => <ProductCard key={id} image={image} title={title} price={price} /> )}
             </ul>
         </section>
     )

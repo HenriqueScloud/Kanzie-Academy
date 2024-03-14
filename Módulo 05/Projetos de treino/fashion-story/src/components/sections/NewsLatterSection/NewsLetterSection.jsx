08a5f5e10606f7aedbc8852069824c1c1b5c76d9
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-14 10:16:04 -0300

    NEW COMMIT - ESTILIZANDO PROJETO FASHION STORY ( NEWSLATTERSECTION.JSX )

--- Módulo 05/Projetos de treino/fashion-story/src/components/sections/NewsLatterSection/NewsLetterSection.jsx
+++ Módulo 05/Projetos de treino/fashion-story/src/components/sections/NewsLatterSection/NewsLetterSection.jsx
@@ -1,14 +1,13 @@
-
 import style from "./style.module.css";
 
 export const NewsLatterSection = () =>{
     return (
-        <form  className={style.newslatterSection}>
+        <form  className={style.form}>
 
             <label htmlFor="newslatter"  className='title2'>INSCREVA-SE NA NEWSLETTE</label>
             <input type="email" name="newslater" id="newslatter" placeholder="E-MAIL" />
             
-            <button>CADASTRE-SE</button>
+            <button className="btn">CADASTRE-SE</button>
         </form>
     )
 }

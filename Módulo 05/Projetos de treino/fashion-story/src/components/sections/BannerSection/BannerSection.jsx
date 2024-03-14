1a0b8b1fe2e9f7c83b49d011391ae262bee0867c
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-13 22:19:15 -0300

    NEW COMMIT - AULA 13/03 ( BANNERSECTION.JSX )

--- Módulo 05/Projetos de treino/fashion-story/src/components/sections/BannerSection/BannerSection.jsx
+++ Módulo 05/Projetos de treino/fashion-story/src/components/sections/BannerSection/BannerSection.jsx
@@ -1,17 +1,15 @@
+import bannerImg from "../../../assets/image/banner.png";
+import style from "./style.module.css";
 
-import bannerImg from "../../../assets/image/banner.png"
-
-export const BannnerSection = () =>{
+export const BannnerSection = () => {
     return (
-        <section>
-            <div>
-                <img src={bannerImg} alt="Imagem do produto" />
-                <div>
-                    <h1>FASHION STORY</h1>
-                    <p>Fique por dentro das nossas últimas promoções e novidades </p>
-                    <button>CONFIRA AS OFERTAS</button>
-                </div>
-            </div>
+        <section className={style.bannerSection}>
+        <img src={bannerImg} alt="Imagem do produto" />
+        <div>
+            <h1 className='title1'>FASHION STORY</h1>
+            <p  className='paragraph'>Fique por dentro das nossas últimas promoções e novidades </p>
+            <button>CONFIRA AS OFERTAS</button>
+        </div>
         </section>
-    )
-}
+    );
+};

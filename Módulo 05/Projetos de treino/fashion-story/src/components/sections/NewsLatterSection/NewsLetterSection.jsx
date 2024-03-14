f3a5e9439dec4ef766cd74451cd5a4addf9c45e0
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-13 22:19:26 -0300

    NEW COMMIT - AULA 13/03 ( NEWSLATTER.JSX )

--- Módulo 05/Projetos de treino/fashion-story/src/components/sections/NewsLatterSection/NewsLetterSection.jsx
+++ Módulo 05/Projetos de treino/fashion-story/src/components/sections/NewsLatterSection/NewsLetterSection.jsx
@@ -1,8 +1,12 @@
+
+import style from "./style.module.css";
+
 export const NewsLatterSection = () =>{
     return (
-        <form>
-            <h2>INSCREVA-SE NA NEWSLETTER</h2>
-            <input type="email" name="" id="" placeholder="E-MAIL" />
+        <form  className={style.newslatterSection}>
+
+            <label htmlFor="newslatter"  className='title2'>INSCREVA-SE NA NEWSLETTE</label>
+            <input type="email" name="newslater" id="newslatter" placeholder="E-MAIL" />
             
             <button>CADASTRE-SE</button>
         </form>

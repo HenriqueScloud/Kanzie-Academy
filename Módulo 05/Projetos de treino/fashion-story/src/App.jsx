58f3d7ae05d9e2a06b9ce5f1557ad381fa97cee1
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-13 22:15:22 -0300

    NEW COMMIT - AULA 13/03 ( APP.JSX )

--- Módulo 05/Projetos de treino/fashion-story/src/App.jsx
+++ Módulo 05/Projetos de treino/fashion-story/src/App.jsx
@@ -1,31 +1,14 @@
+import { HomePage } from "./pages/HomePage";
 import "./styles/index.css";
 
 
-import { Header } from "./components/Header/Header";
-import { BannnerSection } from "./components/sections/BannerSection/BannerSection";
-import { ProductSection } from "./components/sections/ProductsSection/ProductsSection";
-import { NewsLatterSection } from "./components/sections/NewsLatterSection/NewsLetterSection";
-import { Footer } from "./components/Footer/Footer";
-
 
 function App() {
-
   return (
     <>
-    <h1>ola mundo </h1>
-
-      <Header/>
-
-      <main>
-
-        <BannnerSection />
-        <ProductSection />
-        <NewsLatterSection />
-      </main>
-
-      <Footer/>
+      <HomePage/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

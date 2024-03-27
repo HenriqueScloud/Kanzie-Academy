a2381c808f3721f9668688ea692b99726c4d2521
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-03-26 22:23:00 -0300

    NEW COMMIT - PROJETO 1

---
+++ Módulo 05/Projetos de treino/projeto-form-text/Arquivo-base/src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header/Header";
+import { SectionForm } from "./components/Sections/SectionForm/SectionForm";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <SectionForm/>
+    </>
+  );
+}
+
+export default App;

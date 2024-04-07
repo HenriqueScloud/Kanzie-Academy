b1048bf69d737b817522f7fdab488eea3cc67abc
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-04-07 13:00:38 -0300

    NEW POST - DELETE

---
+++ Módulo 05/TCM/Nu-Kenzie/src/components/FInanceList/FinanceList.jsx
@@ -0,0 +1,24 @@
+import { FinanceCard } from "./FinanceCard/FinanceCard";
+import style from './style.module.css'
+
+export const FinanceList = ({listCardTransations, removeCard})=>{
+
+    return(
+
+        
+        <ul className={style.financeList}>
+            <h3 className={style.title}>Resumo Financeiro</h3>
+            {listCardTransations.length === 0 &&(
+                <h2>Você ainda não possui lançamentos </h2>
+            )}
+
+
+            {listCardTransations.map((card)=>{
+                return(
+                <FinanceCard id={card.id} descricao={card.descricao} type={card.type} valor={card.Valor} key={card.id} removeCard={removeCard} /> 
+                )
+            }
+            )}
+        </ul>
+    )
+} 

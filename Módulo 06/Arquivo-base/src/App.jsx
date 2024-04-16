83e6866a4bc59b6247e5cdfe69bf114952cacb27
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-04-15 21:09:09 -0300

    nova atualização de branch

--- Módulo 06/Arquivo-base/src/App.jsx
+++ Módulo 06/Arquivo-base/src/App.jsx
@@ -1,10 +1,18 @@
+import { GlobalStyles } from "./styles/global.js";
+import { Header } from "./components/Header/Header.jsx";
+
 function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <p>Welcome to Next.</p>
-    </>
-  );
+    return (
+        <>
+        <Header/>
+        <GlobalStyles/>
+        <h1>Hello World!</h1>
+        <p>Welcome to Next.</p>
+        <button>Click me</button>
+        <p>teste de branch</p>
+        {/* atualização  */}
+        </>
+    );
 }
 
 export default App;

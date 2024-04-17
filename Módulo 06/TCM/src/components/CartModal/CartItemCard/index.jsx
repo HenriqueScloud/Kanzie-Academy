3701c3ea3dae6111b47ca7d0821adac17c03905c
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-04-17 19:42:41 -0300

    NEW COMMIT - ARQUIVOS JSX

--- Módulo 06/TCM/src/components/CartModal/CartItemCard/index.jsx
+++ Módulo 06/TCM/src/components/CartModal/CartItemCard/index.jsx
@@ -1,15 +1,16 @@
 import { MdDelete } from "react-icons/md";
+import style from "../CartItemCard/style.module.scss"
 
 export const CartItemCard = ({ product }) => {
-   return (
-      <li>
-         <div>
-            <img src={product.img} alt={product.name} />
-            <h3>{product.name}</h3>
-         </div>
-         <button aria-label="delete" title="Remover item">
-            <MdDelete size={21} />
-         </button>
-      </li>
-   );
+    return (
+        <li className={style.CartItemCard}>
+            <div>
+                <img src={product.img} alt={product.name}/>
+                <h3>{product.name}</h3>
+            </div>
+            <button aria-label="delete" title="Remover item">
+                <MdDelete size={21} />
+            </button>
+        </li>
+    );
 };

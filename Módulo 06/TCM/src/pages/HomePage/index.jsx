f38c378fbf547e37e65827e50cf19bba18fde243
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-05-01 20:11:31 -0300

    new 2

--- Módulo 06/TCM/src/pages/HomePage/index.jsx
+++ Módulo 06/TCM/src/pages/HomePage/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
-import { api } from "../../services/DataTeste";
+import { api } from "../../services/Data";
 
 export const HomePage = () => {
     const [productList, setProductList] = useState([]);

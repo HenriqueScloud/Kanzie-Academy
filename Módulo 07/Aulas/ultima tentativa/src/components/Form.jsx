d2a046b19317c85c91863143217f6c17fdad675c
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-05-07 21:15:13 -0300

    new

---
+++ Módulo 07/Aulas/ultima tentativa/src/components/Form.jsx
@@ -0,0 +1,46 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./formSchema";
+import React from "react";
+
+
+export const Form = () =>{
+
+  const {register, handleSubmit,formState: {errors}} = useForm({
+    resolver:zodResolver(formSchema)
+  })
+
+
+  const submit = (formData) =>{
+    console.log(formData);
+    alert('sua mensagem foi enviada com sucesso !')
+
+  }
+
+
+
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <input type="text" placeholder="seu nome" {...register("name")} />
+      {errors.name ? <p> {errors.name.message} </p> : null}
+      <input type="email" placeholder="seu email" {...register("email")} />
+      {errors.email ? <p> {errors.email.message} </p> : null}
+
+      <select {...register("select")}>
+        <option value="">selecione um valor</option>
+        <option value="ensino">ensino</option>
+        <option value="design">design </option>
+        <option value="programação">programação</option>
+      </select>
+      {errors.select ? <p> {errors.select.message} </p> : null}
+
+      <textarea
+        placeholder="sua mensagem !"
+        {...register("massege")}
+      ></textarea>
+      {errors.massege ? <p> {errors.massege.message} </p> : null}
+
+      <button type="submit">enviar</button>
+    </form>
+  );
+}
---
+++ Módulo 07/Aulas/ultima tentativa/src/components/formSchema.js
@@ -0,0 +1,15 @@
+import { z } from "zod"
+
+export const formSchema = z.object({
+  name: z
+    .string()
+    .nonempty("o nome é obrigatorio !")
+    .min(2, "É necessário pelo menos 2 caracteres !"),
+  email: z
+    .string()
+    .nonempty(" o E-mail é obrigatorio !")
+    .email("Forneça um E-mail válido !"),
+  select: z.string().nonempty(" o modulo é obrigatorio !"),
+  massege:z.string().nonempty("A mensagem e obrigatoria !").min(8,"E necessário 8 digitos !")
+  
+});

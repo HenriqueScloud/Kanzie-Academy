7e4416d142d8cb4db5e945d0277ab9d431fdeaf1
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-06-06 09:42:43 -0300

    NEW COMMIT - PUSH

---
+++ Módulo 08/Aulas/semana-01/s1-11( ATIVIDADE CRIANDO UM PRODUTO )/src/produts.ts
@@ -0,0 +1,6 @@
+export interface Iproduct {
+  id: number;
+  name: string;
+  price: number;
+}
+/// criei uma forma de dado onde posso manipular da forma que mais preciso. posso usar como um unico objeto ou transformar um array contendo varios dados desse mesmo tipo usando apenas o dado[]

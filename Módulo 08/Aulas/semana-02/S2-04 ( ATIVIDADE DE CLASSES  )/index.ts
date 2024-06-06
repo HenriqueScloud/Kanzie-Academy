5f2ea2de1cf86b02b4d3474bd5dd38f2a14d68e7
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-06-06 10:07:07 -0300

    NEW COMMIT - PUSH

---
+++ Módulo 08/Aulas/semana-02/S2-04 ( ATIVIDADE DE CLASSES  )/index.ts
@@ -0,0 +1,30 @@
+class student {
+  name:string;
+  module:string;
+  grade: any = null;
+
+  constructor(name:string, module:string){
+    this.name = name;
+    this.module = module
+  }
+
+
+  sayHello(){
+    console.log(`Olá ! Eu sou o ${this.name} e estou no ${this.module}`);
+  }
+
+  setGrade(grade:number){
+    this.grade = grade;
+  }
+  
+  getGrade(){
+    console.log(`a nota do(a) ${this.name} foi ${this.grade}`)
+  }
+}
+
+const user1 = new student('Henrique', 'Módulo 4')
+user1.setGrade(0)
+user1.getGrade();
+
+
+

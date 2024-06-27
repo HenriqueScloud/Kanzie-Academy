53dedd3b3fbb09e426a3eb0e8121a29d8f5abf02
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-06-27 14:31:50 -0300

    NEW COMMIT - TRATANDO DOS ERROS DE REQUISIÇÃO

--- Módulo 08/Projetos de treino/s4-051/src/app.ts
+++ Módulo 08/Projetos de treino/s4-051/src/app.ts
@@ -1,6 +1,7 @@
-
+import "express-async-errors"
 import express, { json } from "express"
 import { userRouter } from "./routers/user.route"
+import { HandleErrors } from "./errors/HandleErrors.middlewares"
 
 export const app = express()
 app.use(json())
@@ -11,3 +12,5 @@ app.use(json())
 // rotas principais para a minha aplicações 
 app.use("/users", userRouter)
 
+// tratamento de erros de servidores 
+app.use(HandleErrors.execute)
--- Módulo 08/Projetos de treino/s4-051/src/middlewares/user.middleware.ts
+++ Módulo 08/Projetos de treino/s4-051/src/middlewares/user.middleware.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { UsersDataBase } from "../dataBase/user.dataBase";
+import { AppErrors } from "../errors/errors";
 
 export class IsUserMiddlewares {
   email(req: Request, res: Response, next: NextFunction) {
     if (UsersDataBase.some((user) => user.email === req.body.email)) {
-      return res.status(409).json({ message: "Email já cadastrado" });
+      throw new AppErrors(409,"Email já cadastrado")
     }
     next();
   }

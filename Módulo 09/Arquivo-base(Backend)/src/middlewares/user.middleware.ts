4caf5f2962ecc2adb8c87495d71b54f764d8e176
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-07-01 16:57:34 -0300

    NEW COMMIT -  trabalhando com errors, middlewares e zod de forma escalavel

--- Módulo 09/Arquivo-base(Backend)/src/middlewares/user.middleware.ts
+++ Módulo 09/Arquivo-base(Backend)/src/middlewares/user.middleware.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { UsersDataBase } from "../dataBase/user.dataBase";
 import { AppErrors } from "../errors/errors";
-
 export class IsUserMiddlewares {
   email(req: Request, res: Response, next: NextFunction) {
-    if (UsersDataBase.some((user) => user.email === req.body.email)) {
-      throw new AppErrors(409, "Email já cadastrado");
+    if (UsersDataBase.some((user) => user.email === req.body.email)) {      
+      throw res.status(409).json({
+        message: "Email já cadastrado",
+      })
     }
     next();
   }

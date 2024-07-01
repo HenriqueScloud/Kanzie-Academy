f6f151fb5e49a8034e924d2c4960ff794a619680
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-07-01 16:57:40 -0300

    NEW COMMIT -  trabalhando com errors, middlewares e zod de forma escalavel

--- Módulo 09/Arquivo-base(Backend)/src/routers/user.route.ts
+++ Módulo 09/Arquivo-base(Backend)/src/routers/user.route.ts
@@ -1,3 +1,4 @@
+import { isCreateProductBodySchameValid } from './../middlewares/isCreateProductBodySchameValid.middleware';
 import { Router } from "express";
 import { userControllers } from "../controllers/user.controllers";
 import { IsUserMiddlewares } from "../middlewares/user.middleware";
@@ -5,7 +6,8 @@ export const userRouter = Router();
 
 const UserControllers = new userControllers();
 const userMiddleware = new IsUserMiddlewares();
+const IsCreateProductBodySchameValid = new isCreateProductBodySchameValid()
 
 userRouter.get("/", UserControllers.getUser);
-userRouter.post("/", userMiddleware.email, UserControllers.createUser);
+userRouter.post("/",userMiddleware.email, IsCreateProductBodySchameValid.execute,  UserControllers.createUser);
 userRouter.delete("/:id", UserControllers.deleteUser);

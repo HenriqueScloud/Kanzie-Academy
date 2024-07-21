625acf47b0f259ed047d5b115ea16e877aa6814b
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-07-20 21:55:03 -0300

    NEW COMMIT

---
+++ Módulo 09/Aulas/semana-03/S3-00/createDatabase.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE m4_aprendendo_slq;
+
+DROP DATABASE name_database;
+/*
+    \l - lista de todos os bancos de dados 
+    \c nome_database - seleciona um banco de dados 
+
+
+*/
+
+
+-- criando o banco de dados "products"

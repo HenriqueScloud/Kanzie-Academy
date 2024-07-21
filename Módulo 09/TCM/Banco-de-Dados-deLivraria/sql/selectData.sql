17e3240b33eca28bf6ea9d59d5e36c14881628f9
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-07-20 21:56:19 -0300

    NEW COMMIT

---
+++ Módulo 09/TCM/Banco-de-Dados-deLivraria/sql/selectData.sql
@@ -0,0 +1,13 @@
+
+
+
+-- leitura de dados:
+--todos os livros 
+-- todas as cateorias de fantasia
+-- todos os livros com suas categorias 
+-- livro harry potter com autos 
+
+
+
+SELECT * FROM books;
+SELECT fantasia FROM categories;

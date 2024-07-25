608cfb8d9fd8f9add6fe1894121c54ce44f10ae9
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-07-25 16:00:12 -0300

    NEW COMMIT

---
+++ Módulo 09/TCM/Banco-de-Dados-deLivraria/banco de dados de livraria/deleteData.sql
@@ -0,0 +1,3 @@
+DELETE FROM books WHERE "name" = 'Jogos Vorazes' RETURNING *;
+DELETE FROM authors WHERE "name" = 'J. K. Rowling' RETURNING *;
+DELETE FROM authors WHERE "name" = 'Osvaldo Silva' RETURNING *;

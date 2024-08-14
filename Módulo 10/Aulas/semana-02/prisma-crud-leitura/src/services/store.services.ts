5e86eb698535ad719142e9c45592dd6c734b5788
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-08-13 22:31:07 -0300

    new commit

---
+++ Módulo 10/Aulas/semana-02/prisma-crud-leitura/src/services/store.services.ts
@@ -0,0 +1,27 @@
+export class StoreServices{
+
+  async createOne(){
+    //criando nova loja
+  }
+  async createMany(){
+    // criando varios lojas de uma so vez 
+  }
+  async findOne(){
+    // leitura lojas por id 
+  }
+  async findMany(){
+    // leitura de varias lojas 
+  }
+  async updateOne(){
+    // atualizando uma loja por id 
+  }
+  async updateMany(){
+    //atualizando TODASSSS as lojas 
+  }
+  async deleteOne(){
+    // deletando uma loja por id
+  }
+  async deleteMany(){
+    // deletando todas as lojas
+  }
+}
